d8529f7bc8d63b9d54b0504c7db00f884dfdc1dd
phongluu11 <mrphong678@gmail.com>
2018-08-29 09:28:27 +0700

    done projcet backup to git

---
+++ app/Models/User.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Prettus\Repository\Contracts\Transformable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Prettus\Repository\Traits\TransformableTrait;
+use Zizaco\Entrust\Traits\EntrustUserTrait;
+
+/**
+ * Class User.
+ *
+ * @package namespace App\Models;
+ */
+class User extends  Authenticatable implements Transformable
+{
+    use TransformableTrait, EntrustUserTrait;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password','group','job','phone','skype','address','birthday'];
+
+    public function showRole(){
+        $roles = Role::all();
+        return $roles;
+    }
+
+    public function votes()
+    {
+        return $this->belongsToMany('App\Models\VoteValue','vote_value_user','user_id','vote_value_id');
+    }
+
+    public function tickets()
+    {
+        return $this->hasMany(Ticket::class);
+    }
+}
